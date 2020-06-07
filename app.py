f7ae6568e583d3808dc6195c24340a0c2f934858
fj3 <lin537x@gmail.com>
2020-06-07 22:26:56 +0800

    refactor: update logging format.

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+import traceback
+from time import strftime
+from flask import request
+
 import crawler
 
 app = crawler.create_app()
+
+crawler.register_blueprints(app)
+
+logger = crawler.register_logging()
+
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    """
+    Logging after every Exception.
+    """
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    tb = traceback.format_exc()
+    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+                 ts,
+                 request.remote_addr,
+                 request.method,
+                 request.scheme,
+                 request.full_path,
+                 tb)
+    return "Internal Server Error", 500
--- crawler/__init__.py
+++ crawler/__init__.py
@@ -9,30 +9,23 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('crawler.settings.default')
 
-    _register_blueprints(app)
-
     return app
 
 
-def _register_blueprints(app):
+def register_blueprints(app):
     from crawler.endpoints import BP as root
 
     app.register_blueprint(root)
 
 
-def _register_logging():
+def register_logging():
+    LOG_FORMAT = '[%(threadName)s]-[%(thread)d] %(asctime)s [%(levelname)s] %(message)s'
+    LOG_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
     timestamp = datetime.now().strftime("%Y%m%d")
     formatter = logging.Formatter(datefmt=LOG_DATE_FORMAT, fmt=LOG_FORMAT)
     handler = logging.FileHandler('log/flask.' + timestamp + '.log', encoding='UTF-8')
     handler.setFormatter(formatter)
     logger = logging.getLogger()
     logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     return logger
-
-
-LOG_FORMAT = '[%(asctime)s - %(levelname)s] %(message)s'
-LOG_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-
-logger = _register_logging()
-
